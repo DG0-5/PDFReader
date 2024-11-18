247e6d202f4db92e7aa1f12a531c0bbfca048888
Dinesh Godhani <dg0.5@outlook.com>
2024-11-18 14:35:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_retrieval_chain, create_history_aware_retriever
-# from langchain_chroma import Chroma
+from langchain_chroma import Chroma
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_text_splitters import RecursiveCharacterTextSplitter
